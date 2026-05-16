534eedba51d48d948f359059e215e28a0720d91d
Wafiq <wafiqnura82@gmail.com>
2026-05-17 02:26:04 +0700

    fix syntax

--- app.js
+++ app.js
@@ -53,11 +53,6 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
-.catch((err) => {
-
-    console.log(err);
-
-});
 
 // =======================
 // MIDDLEWARE LOGIN ADMIN
