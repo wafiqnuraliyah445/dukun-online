5cfb6e96b4ca5e17f007ebc47ec1ab50b7e1d579
Wafiq <wafiqnura82@gmail.com>
2026-05-17 02:15:57 +0700

    mongodb atlas

--- app.js
+++ app.js
@@ -49,17 +49,9 @@ app.use(session({
 // MONGODB
 // =======================
 
-mongoose.connect(
-    'mongodb://127.0.0.1:27017/dukun_online_db'
-)
-
-.then(() => {
-
-    console.log(
-        '✅ MongoDB Connected'
-    );
-
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 .catch((err) => {
 
